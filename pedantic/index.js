d011bc3466e3cab648b2e51aa96c08a8c0fee7a1
Travis Webb <me@traviswebb.com>
2014-07-01 03:02:46 -0400

    bundle and cleanup

--- pedantic/index.js
+++
@@ -1,6 +0,0 @@
-console.log('nex-pedantic');
-
-// try to break node > 0.10. will work in 0.8
-["SIGINT", "SIGHUP", "SIGQUIT", "SIGKILL", "SIGSEGV", "SIGILL"].forEach(function (sig) {
-  process.once(sig, function () { });
-});
